0bbd4eae1e551b53dcfc4da2e9b85fb33d6d5dec
Omar Rahman <omar+github@ramen5914.net>
2025-01-29 06:57:18 -0800

    Added some localization for config menu

---
+++ localization/en-us.lua
@@ -0,0 +1,46 @@
+return {
+    descriptions = {
+        Back = {
+            b_pktr_poker = {
+                name = "Poker Deck",
+                text = {
+                    "No modifiers"
+                }
+            }
+        },
+        Blind = {},
+        Edition = {},
+        Enhanced = {},
+        Joker = {},
+        Other = {},
+        Planet = {},
+        Spectral = {},
+        Stake = {},
+        Tag = {},
+        Tarot = {},
+        Voucher = {},
+    },
+    misc = {
+        achievement_descriptions = {},
+        achievement_names = {},
+        blind_states = {},
+        challenge_names = {},
+        collabs = {},
+        dictionary = {
+            b_pktr_joker_spaces = "Joker Spaces",
+            b_pktr_buy_in = "Buy-In Amount",
+            ml_pktr_buy_in_opt = { "$50", "$100", "$200", "$500" }
+        },
+        high_scores = {},
+        labels = {},
+        poker_hand_descriptions = {},
+        poker_hands = {},
+        quips = {},
+        ranks = {},
+        suits_plural = {},
+        suits_singular = {},
+        tutorial = {},
+        v_dictionary = {},
+        v_text = {},
+    },
+}
