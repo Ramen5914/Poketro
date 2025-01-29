0481d27f6763c15b30298fa3c83a1522fc3d4003
Omar Rahman <omar+github@ramen5914.net>
2025-01-29 07:01:02 -0800

    Added config tab UI code

--- src/main.lua
+++ src/main.lua
@@ -9,3 +9,30 @@ SMODS.Atlas {
     py = 95
 }
 
+SMODS.current_mod.config_tab = function()
+    return {
+        n = G.UIT.ROOT,
+        config = {
+            align = "cm",
+            padding = 0.05,
+            colour = G.C.CLEAR
+        },
+        nodes = {
+            create_option_cycle({
+                label = localize('b_pktr_joker_spaces'),
+                scale = 0.8,
+                options = { 0, 1, 2, 3, 4, 5 },
+                opt_callback = 'change_joker_spaces',
+                current_option = (Poketro.config['joker_spaces'] + 1)
+            }),
+            create_option_cycle({
+                label = localize('b_pktr_buy_in'),
+                scale = 0.8,
+                options = localize('ml_pktr_buy_in_opt'),
+                opt_callback = 'change_buy_in_amount',
+                current_option = Poketro.config['buy_in_amount']
+            }),
+        }
+    }
+end
+
