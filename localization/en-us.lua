e1da1b6a615533379050146c5dccfdb62f2e4731
Omar Rahman <omar+github@ramen5914.net>
2025-01-30 16:29:12 -0800

    Added more localization

--- localization/en-us.lua
+++ localization/en-us.lua
@@ -29,6 +29,10 @@ return {
         dictionary = {
             b_pktr_joker_spaces = "Joker Spaces",
             b_pktr_buy_in = "Buy-In Amount",
+            b_pktr_rebuy_limit = "Rebuy Limit",
+            b_pktr_share_jokers = "Share Jokers",
+            b_pktr_share_hand_levels = "Share Hand Levels",
+            b_pktr_poker = "Poketro",
             ml_pktr_buy_in_opt = { "$50", "$100", "$200", "$500" }
         },
         high_scores = {},
