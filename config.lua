e9dfbe98dabec1b43f68387f8c3da969cdf8e69a
Omar Rahman <omar+github@ramen5914.net>
2025-01-30 16:28:54 -0800

    Added new config options

--- config.lua
+++ config.lua
@@ -1,4 +1,7 @@
 return {
-    ["joker_spaces"] = 3,
+    ["joker_spaces"] = 5,
     ["buy_in_amount"] = 2,
+    ["rebuy_limit"] = 1,
+    ["share_jokers"] = false,
+    ["share_hand_levels"] = true
 }
