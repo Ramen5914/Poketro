26775acdee328c05f4c1958334ae64020f821058
Omar Rahman <omar+github@ramen5914.net>
2025-01-30 16:27:50 -0800

    Added UI code

--- src/main.lua
+++ src/main.lua
@@ -40,26 +40,66 @@ SMODS.Atlas {
 SMODS.current_mod.config_tab = function()
     return {
         n = G.UIT.ROOT,
-        config = {
-            align = "cm",
-            padding = 0.05,
-            colour = G.C.CLEAR
-        },
+        config = { r = 0.1, minw = 8, minh = 6, align = "tm", padding = 0.2, colour = G.C.BLACK },
         nodes = {
-            create_option_cycle({
-                label = localize('b_pktr_joker_spaces'),
-                scale = 0.8,
-                options = { 0, 1, 2, 3, 4, 5 },
-                opt_callback = 'change_joker_spaces',
-                current_option = (Poketro.config['joker_spaces'] + 1)
-            }),
-            create_option_cycle({
-                label = localize('b_pktr_buy_in'),
-                scale = 0.8,
-                options = localize('ml_pktr_buy_in_opt'),
-                opt_callback = 'change_buy_in_amount',
-                current_option = Poketro.config['buy_in_amount']
-            }),
+            {
+                n = G.UIT.C,
+                config = { minw = 4, minh = 4, colour = G.C.CLEAR, align = "tm", padding = 0.15 },
+                nodes = {
+                    create_option_cycle({
+                        label = localize('b_pktr_joker_spaces'),
+                        scale = 0.8,
+                        options = { 0, 1, 2, 3, 4, 5 },
+                        opt_callback = 'change_joker_spaces',
+                        current_option = (Poketro.config['joker_spaces'] + 1)
+                    }),
+                    {
+                        n = G.UIT.R,
+                        config = { minw = 2, minh = 1, align = "tm", padding = 0.15 },
+                        nodes = {
+                            {
+                                n = G.UIT.C,
+                                config = {},
+                                nodes = {
+                                    create_option_cycle({
+                                        label = localize('b_pktr_buy_in'),
+                                        scale = 0.8,
+                                        options = localize('ml_pktr_buy_in_opt'),
+                                        opt_callback = 'change_buy_in_amount',
+                                        current_option = Poketro.config['buy_in_amount']
+                                    })
+                                }
+                            },
+                            {
+                                n = G.UIT.C,
+                                config = {},
+                                nodes = {
+                                    create_option_cycle({
+                                        label = localize('b_pktr_rebuy_limit'),
+                                        scale = 0.8,
+                                        options = { 0, 1, 2, 3 },
+                                        opt_callback = 'change_rebuy_limit',
+                                        current_option = (Poketro.config['rebuy_limit'] + 1)
+                                    })
+                                }
+                            }
+                        }
+                    },
+                    create_toggle({
+                        label = localize('b_pktr_share_jokers'),
+                        toggle_callback = G.FUNCS.toggle_share_jokers,
+                        ref_table = Poketro.config,
+                        ref_value = 'share_jokers',
+                        config = { align = "tm" }
+                    }),
+                    create_toggle({
+                        label = localize('b_pktr_share_hand_levels'),
+                        toggle_callback = G.FUNCS.toggle_share_hand_levels,
+                        ref_table = Poketro.config,
+                        ref_value = 'share_hand_levels'
+                    })
+                }
+            }
         }
     }
 end
@@ -75,3 +115,36 @@ SMODS.Back {
     discovered = true,
     omit = true
 }
+
+function G.UIDEF.poketro()
+    return (
+        create_UIBox_generic_options({
+            contents = {
+                UIBox_button({
+                    label = { G.localization.misc.dictionary["singleplayer"] or "Singleplayer" },
+                    colour = G.C.BLUE,
+                    button = "setup_run",
+                    minw = 5,
+                }),
+                true and UIBox_button({
+                    label = { G.localization.misc.dictionary["create_lobby"] or "Create Lobby" },
+                    colour = G.C.GREEN,
+                    button = "create_lobby",
+                    minw = 5,
+                }) or nil,
+                true and UIBox_button({
+                    label = { G.localization.misc.dictionary["join_lobby"] or "Join Lobby" },
+                    colour = G.C.RED,
+                    button = "join_lobby",
+                    minw = 5,
+                }) or nil,
+                not true and UIBox_button({
+                    label = { G.localization.misc.dictionary["reconnect"] or "Reconnect" },
+                    colour = G.C.RED,
+                    button = "reconnect",
+                    minw = 5,
+                }) or nil,
+            },
+        })
+    )
+end
